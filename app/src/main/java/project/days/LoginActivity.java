1dbb479c67c19f0c6508465ac46520df47cf4888
peeyon <70425128+peeyon@users.noreply.github.com>
2020-08-29 23:55:47 +0530

    Added functionality to the login page -Sanjay

--- app/src/main/java/project/days/LoginActivity.java
+++ app/src/main/java/project/days/LoginActivity.java
@@ -1,14 +1,112 @@
 package project.days;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    String email, password;
+    private EditText EmailET, PasswordET;
+    private Button LoginButton;
+    private TextView ForgotLink, signupLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        EmailET = (EditText) findViewById(R.id.email_et);
+        PasswordET = (EditText) findViewById(R.id.password_et);
+        LoginButton = (Button) findViewById(R.id.login_button);
+        ForgotLink = (TextView) findViewById(R.id.forget_password_tv);
+        signupLink = (TextView) findViewById(R.id.sign_up_tv);
+
+        LoginButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                email = EmailET.getText().toString();
+                password = PasswordET.getText().toString();
+                if(!(TextUtils.isEmpty(email) && TextUtils.isEmpty(password)))
+                {
+                    mAuth.signInWithEmailAndPassword(email,password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful())
+                                    {
+                                        Toast.makeText(LoginActivity.this, "Authenticated successfully", Toast.LENGTH_SHORT).show();
+                                        Intent mainIntent = new Intent(LoginActivity.this,MainActivity.class);
+                                        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(mainIntent);
+                                    }
+                                    else
+                                    {
+                                        String message = task.getException().getMessage().toString();
+                                        Toast.makeText(LoginActivity.this, "Error occured. "+message, Toast.LENGTH_SHORT).show();
+                                    }
+
+                                }
+                            });
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this, "Please fill all the credentials", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        ForgotLink.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                email = EmailET.getText().toString();
+                if(TextUtils.isEmpty(email))
+                {
+                    Toast.makeText(LoginActivity.this, "Please provide your email address to reset your password", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful())
+                            {
+                                Toast.makeText(LoginActivity.this, "Password reset link has been sent to your inbox.", Toast.LENGTH_SHORT).show();
+                            }
+                            else
+                            {
+                                String message = task.getException().getMessage().toString();
+                                Toast.makeText(LoginActivity.this, "Error occured. "+message, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        signupLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signupIntent =  new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(signupIntent);
+            }
+        });
+
     }
 }
