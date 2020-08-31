0e29b9ab81cf3f533b2519f16964b976a5411d58
peeyon <pranavanpeeyon@gmail.com>
2020-08-31 05:17:36 -0700

    Added Personal details info page UI-Pranavan making buttons in a more innovative manner will be done afterwards

---
+++ app/src/main/java/project/days/PersonalDetailsActivity.java
@@ -0,0 +1,14 @@
+package project.days;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonalDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_details);
+    }
+}
