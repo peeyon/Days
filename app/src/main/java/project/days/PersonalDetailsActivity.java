a7b4125fdd83deeda935bc0d1fb132dbe6c3c9f4
peeyon <70425128+peeyon@users.noreply.github.com>
2020-09-01 10:49:56 +0530

    - Added Date selection functionality --Sanjay

--- app/src/main/java/project/days/PersonalDetailsActivity.java
+++ app/src/main/java/project/days/PersonalDetailsActivity.java
@@ -2,19 +2,29 @@ package project.days;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Calendar;
 
 public class PersonalDetailsActivity extends AppCompatActivity {
 
     private EditText NameET, NickNameET;
-    private TextView MaleT, FemaleT, OtherT;
+    private TextView MaleT, FemaleT, OtherT, DateT;
     private Button NextButton;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
     boolean maleb,femaleb, otherb;
-    String name,nickname,gender;
+    String name,nickname,gender,date;
+    int year,month,day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,36 @@ public class PersonalDetailsActivity extends AppCompatActivity {
         MaleT = (TextView) findViewById(R.id.male_select);
         FemaleT = (TextView) findViewById(R.id.female_select);
         OtherT = (TextView) findViewById(R.id.other_select);
+        DateT = (TextView) findViewById(R.id.pd_dob);
+
+        DateT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                year = cal.get(Calendar.YEAR);
+                month = cal.get(Calendar.MONTH);
+                day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        PersonalDetailsActivity.this,
+                        android.R.style.Theme_Material_Light_Dialog,
+                        mDateSetListener,
+                        day,month,year
+                );
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+                dialog.show();
+            }
+        });
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                year = i;
+                month = i1+1;
+                day = i2;
+                date = day + "/" + month + "/" + year;
+                DateT.setText(date);
+            }
+        };
 
 
         MaleT.setOnClickListener(new View.OnClickListener() {
