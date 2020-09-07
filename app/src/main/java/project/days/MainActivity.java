275443ef8324337ac7294e2b643c41f7b4141797
Sanjay Thiyagarajan <42594454+sanjay-thiyagarajan@users.noreply.github.com>
2020-09-07 23:56:39 +0530

    UI building Stage-2 DiaryViewActivity

--- app/src/main/java/project/days/MainActivity.java
+++ app/src/main/java/project/days/MainActivity.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -22,13 +23,38 @@ public class MainActivity extends AppCompatActivity {
 
     private CardView personalCard,groupCard;
     private ImageView settingsIcon;
-
+    private FirebaseAuth mAuth;
+    String nickname,currentUserID;
+    private DatabaseReference usersRef;
+    private TextView greetText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        greetText = (TextView) findViewById(R.id.main_greet_1);
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+        usersRef = FirebaseDatabase.getInstance().getReference("Users").child(currentUserID);
+        usersRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.hasChild("Nickname"))
+                {
+                    nickname = snapshot.child("Nickname").getValue().toString();
+                }
+                else
+                {
+                    nickname = snapshot.child("Name").getValue().toString();
+                }
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        greetText.setText("Hello " + nickname);
         personalCard = (CardView) findViewById(R.id.inner_card_1);
         groupCard = (CardView) findViewById(R.id.inner_card_2);
         settingsIcon = (ImageView) findViewById(R.id.settings_icon_main);
