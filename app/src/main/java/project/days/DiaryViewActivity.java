742a72e40dcf5d3d59eb79430796a11133ea37a7
Sanjay Thiyagarajan <42594454+sanjay-thiyagarajan@users.noreply.github.com>
2020-09-07 23:02:50 +0530

    UI building Stage-2 DiaryViewActivity

---
+++ app/src/main/java/project/days/DiaryViewActivity.java
@@ -0,0 +1,14 @@
+package project.days;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DiaryViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diary_view);
+    }
+}
--- app/src/main/java/project/days/MainActivity.java
+++ app/src/main/java/project/days/MainActivity.java
@@ -2,8 +2,13 @@ package project.days;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageHelper;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -15,12 +20,40 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    private CardView personalCard,groupCard;
+    private ImageView settingsIcon;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        personalCard = (CardView) findViewById(R.id.inner_card_1);
+        groupCard = (CardView) findViewById(R.id.inner_card_2);
+        settingsIcon = (ImageView) findViewById(R.id.settings_icon);
+
+        settingsIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent settingsIntent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(settingsIntent);
+            }
+        });
+
+        personalCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        groupCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
     }
 }
