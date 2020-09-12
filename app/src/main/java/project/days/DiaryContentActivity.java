5b10baa45122ec73378e53627a367f8203dc957c
Naresh Kumar <60980410+techienk@users.noreply.github.com>
2020-09-12 21:07:48 +0530

    Update in - DiaryContentActivity.java

--- app/src/main/java/project/days/DiaryContentActivity.java
+++ app/src/main/java/project/days/DiaryContentActivity.java
@@ -3,12 +3,16 @@ package project.days;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class DiaryContentActivity extends AppCompatActivity {
 
+    private EditText Diaryheading;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary_content);
+        Diaryheading = (EditText) findViewById(R.id.content_diary_name);
+        
     }
 }
