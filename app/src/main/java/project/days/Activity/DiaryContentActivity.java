ba3060cef19721aabcb81f4ac9497ffcb4448bee
Techipeeyon <70622125+techipeeyon@users.noreply.github.com>
2020-09-30 09:20:10 -0700

    Successfully completed 60lines of diary content page and added text box for the content and buttons to add photos and videos

--- app/src/main/java/project/days/Activity/DiaryContentActivity.java
+++ app/src/main/java/project/days/Activity/DiaryContentActivity.java
@@ -1,25 +1,60 @@
-package project.days.Activity;
-
-import android.os.Bundle;
-import android.widget.EditText;
+package project.days;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-
-import project.days.R;
 
 public class DiaryContentActivity extends AppCompatActivity {
+    private EditText mDiaryContent;
+    private Button mContentViewBack;
+    private Button mContentEdit;
+    private Button mContentDelete;
+    private Button mContentAddPhotos;
+    private Button mContentAddVideos;
+    private FirebaseAuth fAuth;
 
-    private EditText Diaryheading;
-    private FirebaseAuth mAuth;
-    private DatabaseReference postRef;
-    String currentUserID,diary_id;
+    public DiaryContentActivity() {
+    }
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary_content);
-        Diaryheading = findViewById(R.id.content_diary_name);
+
+        mContentViewBack = findViewById(R.id.content_view_back_icon);
+        mContentEdit     = findViewById(R.id.content_edit_icon);
+        mContentDelete   = findViewById(R.id.content_delete_icon);
+        mDiaryContent    = findViewById(R.id.diary_content_text);
+        mContentAddPhotos= findViewById(R.id.content_add_photos);
+        mContentAddVideos= findViewById(R.id.content_add_videos);
+
+
+        fAuth = FirebaseAuth.getInstance();
+
+        mContentEdit.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v){
+                                                String diarycontent = mDiaryContent.getText().toString().trim();
+
+                                                if(TextUtils.isEmpty(diarycontent)){
+                                                    mDiaryContent.setError("Enter the content.");
+                                                    return;
+                                                }
+                                            }
+                                        }
+        );
+
+
+
     }
 }
