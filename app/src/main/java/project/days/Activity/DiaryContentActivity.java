8fed278aeffa48324c538fb08223032ab53625db
Xavier-Alfred <71057058+xavier-alfred@users.noreply.github.com>
2020-10-07 16:36:46 +0530

    Code for uploading the images

--- app/src/main/java/project/days/Activity/DiaryContentActivity.java
+++ app/src/main/java/project/days/Activity/DiaryContentActivity.java
@@ -1,10 +1,16 @@
 package project.days;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -12,7 +18,15 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+import java.util.UUID;
 
 public class DiaryContentActivity extends AppCompatActivity {
     private EditText mDiaryContent;
@@ -22,9 +36,15 @@ public class DiaryContentActivity extends AppCompatActivity {
     private Button mContentAddPhotos;
     private Button mContentAddVideos;
     private FirebaseAuth fAuth;
+    private Uri filePath;
+    private FirebaseStorage storage;
+    private StorageReference storageReference;
+
 
     public DiaryContentActivity() {
     }
+
+
 
     @SuppressLint("WrongViewCast")
     @Override
@@ -41,6 +61,8 @@ public class DiaryContentActivity extends AppCompatActivity {
 
 
         fAuth = FirebaseAuth.getInstance();
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
 
         mContentEdit.setOnClickListener(new View.OnClickListener() {
                                             @Override
@@ -59,11 +81,69 @@ public class DiaryContentActivity extends AppCompatActivity {
                                         }
 
         );
-
+        mContentAddPhotos.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void OnClick(View v)
+            {
+                chooseImage();
+                UploadImage();
+            }
+        });
+        private void chooseImage()
         {
+            Intent.intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(intent, title:"Select image"),requestcode:1));
+
+        }
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+        {
+            super.onActivityResult(requestCode, resultCode, data);
+            if(requestCode == 1 && resultCode == RESULT_OK && data!= null && data.getData()!=null)
+
+            {
+                filePath = data.getData();
+                try {
+
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath)
+                    image.View.setImageBitmap(bitmap);
+                } catch (IOException e)
+                {
+                    e,printStackTrace();
+
+                }
+
+            }
 
         }
 
+        private void uploadImage()
+        {
+            if(filePath!=null)
+            { final ProgressDialog progressDialog = new ProgressDialog(content: this)
+                progressDialog.setTitle("Uploading");
+                progressDialog.show();
+                StorageReference reference = storageReference.child("images/" + UUID.randomUUID().toString());
+                reference.putFile((filePath)).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot)
+                    {
+                        progressDialog.dismiss();
+                        Toast.makeText(content: DiaryContentActivity.this, text: "Image Uploaded", );
+                    }
+                }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                        double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot.getTotalByteCount());
+                        progressDialog.setMessage("Uploaded"+(int)progress+"%" );
+
+                    }
+                });
+
+            }
+        }
 
     }
 }
