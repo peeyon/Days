1483752f7a77bf4f8fb6ca023250d8a4678afef9
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2020-11-10 19:33:06 +0530

    Added FriendsActivity  - Sanjay Thiyagarajan

---
+++ app/src/main/java/project/days/Activity/FriendsActivity.java
@@ -0,0 +1,16 @@
+package project.days.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import project.days.R;
+
+public class FriendsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends);
+    }
+}
