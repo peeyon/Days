f16d30f7233db5f0484a7dd5b07ca3ed15248289
Xavier Emmanuel E <xavier12emmanuel@gmail.com>
2020-11-12 00:37:53 +0530

    Added codes for uploading images successfully

--- app/src/main/java/project/days/Activity/DiaryContentActivity.java
+++ app/src/main/java/project/days/Activity/DiaryContentActivity.java
@@ -1,8 +1,15 @@
 package project.days.Activity;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +19,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -19,8 +27,15 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.UUID;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import project.days.R;
@@ -30,11 +45,13 @@ public class DiaryContentActivity extends AppCompatActivity {
 
     EditText DiaryNameTV, DiaryContentET;
     CircleImageView EditButton, DeleteButton;
-    ImageView BackButton;
+    ImageView BackButton,imageView;
     FirebaseAuth mAuth;
     DatabaseReference diariesRef, usersRef;
     String currentUserID, type, diary_id;
-    Button SaveButton;
+    private Uri filePath;
+    Button SaveButton,Photo,upload;
+    private StorageReference storageReference;
 
 @Override
 protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +59,7 @@ protected void onCreate(Bundle savedInstanceState) {
     setContentView(R.layout.activity_diary_content);
     mAuth = FirebaseAuth.getInstance();
     usersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+    storageReference = FirebaseStorage.getInstance().getReference("image");
     currentUserID = mAuth.getCurrentUser().getUid();
     type = getIntent().getStringExtra("type");
     diary_id = getIntent().getStringExtra("diary_id");
@@ -57,6 +75,9 @@ protected void onCreate(Bundle savedInstanceState) {
     DeleteButton = (CircleImageView) findViewById(R.id.content_delete_icon);
     DiaryContentET = (EditText) findViewById(R.id.diary_content_text);
     SaveButton = (Button) findViewById(R.id.save_changes_button);
+    Photo = (Button) findViewById(R.id.content_add_photos);
+    imageView = (ImageView) findViewById(R.id.myImage);
+    upload = (Button) findViewById(R.id.content_add_videos);
 
     diariesRef.child(diary_id).addValueEventListener(new ValueEventListener() {
         @Override
@@ -109,6 +130,18 @@ protected void onCreate(Bundle savedInstanceState) {
         }
     });
 
+    Photo.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            chooseImage();
+        }
+    });
+    upload.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            UploadImage();
+        }
+    });
     BackButton.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -154,6 +187,54 @@ protected void onCreate(Bundle savedInstanceState) {
         }
     });
 
+
 }
+@Override
+protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+    super.onActivityResult(requestCode, resultCode, data);
+    if (requestCode == 1 && resultCode == RESULT_OK && data!=null && data.getData()!= null)
+    {
+        filePath = data.getData();
+        try{
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),filePath);
+            imageView.setImageBitmap(bitmap);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+    private void UploadImage() {
+        if (filePath != null) {
+            final ProgressDialog progressDialog = new ProgressDialog(DiaryContentActivity.this);
+            progressDialog.setTitle("Uploading");
+            progressDialog.show();
+            StorageReference reference = storageReference.child("image/" + UUID.randomUUID().toString());
+            reference.putFile(filePath).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    progressDialog.dismiss();
+                    Toast.makeText(DiaryContentActivity.this, "Image uploaded", Toast.LENGTH_SHORT).show();
+                }
+            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                    double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                    progressDialog.setMessage("Uploaded" + (int) progress + "%");
+
+                }
+            });
+        }
+    }
+
+    private void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select image"), 1);
+    }
+
 
 }
